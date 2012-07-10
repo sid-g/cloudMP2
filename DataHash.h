44d9291527e648a19957b76f359a7d9d32747690
Siddhant Gahlot <gahlot1@illinois.edu>
2012-07-10 02:21:41 -0500

    Siddhant: Added header file for data hash table

---
+++ DataHash.h
@@ -0,0 +1,20 @@
+#ifndef _DataHash_H
+#define _DataHash_H
+
+#include <map>
+
+struct Data {
+	string data;
+}
+
+class DataHash
+{
+private:
+
+public:
+	
+	map<int, Data> hashTable;
+	
+}
+
+#endif

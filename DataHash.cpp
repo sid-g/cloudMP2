0a343d3bf5732de528cbee4defc414f15aa3f940
Siddhant Gahlot <gahlot1@illinois.edu>
2012-07-11 23:38:56 -0500

    Siddhant: added more methods for hash-table

---
+++ DataHash.cpp
@@ -0,0 +1,62 @@
+#include <string>
+
+#include "DataHash.h"
+
+using namespace std;
+
+int DataHash::size() 
+{
+	return hashTable.size();
+}
+
+int insert(int key, Data value)
+{
+	pair<map<int,Data>::iterator,bool> ret;
+	ret = hashTable.insert(pair<int, Data>(key, value));
+	if(ret.second == false) // there is already a value for this key.
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int remove(int key)
+{
+	if(hashTable.erase(key) == 0) // number of elements with this key erased = 0
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int removeSmallest()
+{
+	return -1;
+}
+
+int removeLargest()
+{
+	return -1;
+}
+
+bool exist(int key)
+{
+	//map<int,string>::iterator it;
+	return false;
+}
+
+int insertAndRemoveLargest()
+{
+	return -1;
+}
+
+int insertAndRemoveSmallest()
+{
+	return -1;
+}
--- DataHash.h
+++ DataHash.h
@@ -2,19 +2,62 @@
 #define _DataHash_H
 
 #include <map>
+#include <string>
+
+using namespace std;
 
 struct Data {
 	string data;
-}
+};
 
 class DataHash
 {
 private:
+	
+	map<int, Data> hashTable;
 
 public:
 	
-	map<int, Data> hashTable;
+	/**
+	*	Returns the size of the hash table on this server.
+	*/
+	int size();
 	
-}
+	/**
+	*	
+	*/
+	int insert(int key, Data value);
+	
+	/**
+	*
+	*/
+	int remove(int key);
+	
+	/**
+	*
+	*/
+	int removeSmallest();
+	
+	/**
+	*
+	*/
+	int removeLargest();
+	
+	/**
+	*
+	*/
+	bool exist(int key);
+	
+	/**
+	*
+	*/
+	int insertAndRemoveLargest();
+	
+	/**
+	*
+	*/
+	int insertAndRemoveSmallest();
+	
+};
 
 #endif
